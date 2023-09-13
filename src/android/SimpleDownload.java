8b730789f69814a433da683402c7e71356bb334f
lijiong <ffeiman@163.com>
2023-09-13 14:59:57 +0800

    add file mkdirs

--- src/android/SimpleDownload.java
+++ src/android/SimpleDownload.java
@@ -2,11 +2,13 @@ package dev.tribody.simpledownload;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.LOG;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Files;
@@ -16,13 +18,11 @@ import java.nio.file.StandardCopyOption;
 public class SimpleDownload extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("download")) {
-             String url = args.getString(0);
-             String dest = args.getString(1);
+        if (action.equals("download")) {           
             cordova.getThreadPool().execute(new Runnable() {
                 @Override
                 public void run() {
-                    download(url, dest, callbackContext);
+                    download(args, callbackContext);
                 }
             });
             return true;
@@ -30,13 +30,21 @@ public class SimpleDownload extends CordovaPlugin {
         return false;
     }
     
-    private void download(String url, String dest, CallbackContext callbackContext) {       
+    private void download(JSONArray args, CallbackContext callbackContext) {       
         try{
+            String url = args.getString(0);
+            String dir = args.getString(1);
+            String name = args.getString(2);
+            if(!dir.endsWith("/")) {
+                dir = dir + "/";
+            }
+            File file = new File(dir);
+            if(!file.exists()) {
+                file.mkdirs();
+            }
             InputStream in = new URL(url).openStream();
-            Files.copy(in, Paths.get(dest), StandardCopyOption.REPLACE_EXISTING);
-            
+            Files.copy(in, Paths.get(dir + name), StandardCopyOption.REPLACE_EXISTING);            
             callbackContext.success();
-            
         } catch (Exception e){
             callbackContext.error(e.toString());
         }
--- www/SimpleDownload.js
+++ www/SimpleDownload.js
@@ -1,11 +1,11 @@
 var exec = require('cordova/exec');
 
 module.exports = {
-  download: function (url, dest, onSuccess, onError) {
-    if (!url || !dest) {
-      console.error('download url or dest is null!');
+  download: function (url, dir, name, onSuccess, onError) {
+    if (!url || !dir || !name) {
+      onError('download args is null!');
       return;
     }
-    exec(onSuccess, onError, 'SimpleDownload', 'download', [url, dest]);
+    exec(onSuccess, onError, 'SimpleDownload', 'download', [url, dir, name]);
   },
 };
